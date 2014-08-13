e08efbab476a9185240248961bdd6b3fecd446ae
pscollins@uchicago.edu <pscollins@uchicago.edu>
2014-08-13 02:03:23 -0500

    first pass with some filler info

--- mockchicago/views.py
+++ mockchicago/views.py
@@ -1,7 +1,23 @@
 from flask import request, render_template
 
 from mockchicago.app import app
+from mockchicago.models import Member, Photo
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    all_officers = Member.query.order_by(
+        Member.officer_rank.desc()).limit(5).all()
+
+    # president, officer_members = all_officers[0], all_officers[1:]
+    officer_members = all_officers
+
+    print("Got officers: {}".format(officer_members))
+
+    photos = Photo.query.all()
+
+    print("Got photos: {}".format(photos))
+
+    return render_template("index.html", president=None,
+                           officer_members=officer_members,
+                           members_with_modals=officer_members,
+                           photos=photos)
--- run.py
+++ run.py
@@ -1,4 +1,53 @@
-from mockchicago.app import app
+import argparse
+import json
+
+from mockchicago.app import app, db
+from mockchicago.models import Member
 # from mockchicago import views
 
-app.run(debug=True)
+class BadCommandError(Exception):
+    pass
+
+def init_db(json_file):
+    people = json.load(json_file)
+
+    db.create_all()
+
+    for person in people:
+        print("Adding: {}".format(person))
+        new_person = Member(**person)
+        db.session.add(new_person)
+
+    db.session.commit()
+
+
+if __name__ == "__main__":
+    arg_parser = argparse.ArgumentParser("Interact with the server.")
+    arg_parser.add_argument(
+        "action",
+        metavar="action",
+        nargs=1,
+        help="(run), (init)alize from file, or (gen)erate.")
+    arg_parser.add_argument(
+        "-f",
+        dest="init_file",
+        default=None,
+        help=("Supply a JSON-formatted configuration file"
+              "to initialize the database."))
+
+    args = arg_parser.parse_args()
+
+    print("Got args: {}, action {}".format(args, args.action))
+    print("action eq? {}".format(args.action == "init"))
+
+    action = args.action[0]
+
+    if action == "run":
+        app.run(debug=True, host="0.0.0.0", port=8000)
+    elif action == "init":
+        if args.init_file is not None:
+            init_db(open(args.init_file))
+        else:
+            raise BadCommandError()
+    else:
+        raise BadCommandError()
